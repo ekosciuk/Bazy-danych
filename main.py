963dc8d462a50114b4c21d1ba67bc5c376beda39
ekosciuk <148440605+ekosciuk@users.noreply.github.com>
2023-10-23 13:00:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+a = 5
+print(a)
+b = a
+a = 6
+print(b)
+
+#a = input('Jak masz na imię?')
+#print(a)
+#print(f'Masz na imię {a}')
+#wiek = int(input('Ile masz lat?'))
+#print(f' Masz {wiek} lat')
+
+#a = int(input('Podaj liczbę od 0 do 10'))
+#if a == 5:
+    #print('Jesteś zwycięzcą')
+#else:
+    #print('Przegrałaś')
+    #print('Ha ha ha')
+
+#lista = [1, 3, 8]
+#print(lista)
+#for element in lista:
+    #print(element)
+
+#for i in range(2, 10, 2):
+    #print(i)
+
+suma = 0
+n = 10
+for x in range(n+1):
+    suma += x
+print(suma)
+Zrób sumę kwadratów liczb od 1 do 100 włącznie
+
